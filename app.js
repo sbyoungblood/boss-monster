7790d57a34f76dfa1ceb675aedc7b42dd91ee393
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-01-26 14:06:22 -0700

    html, js, css, hero row

---
+++ app.js
@@ -0,0 +1,22 @@
+let heroes = [
+  {
+      name: 'Slate Slabrock',
+      type: 'dwarf',
+      damage: 5,
+      health: 100
+  },
+  {
+      name: 'Flint Ironstag',
+      type: 'elf',
+      damage: 10,
+      health: 50
+  }
+]
+
+
+let boss = {
+  health: 100,
+  maxHealth: 100,
+  damage: 5,
+  level: 1
+}
